612c0057083761420fcd21f97c85cee545ed3db7
Johannes Boehm <johannes.boehm@gmail.com>
2017-10-17 00:07:02 -0400

    Some work on setup.jl and StataCall.jl

--- src/StataCall.jl
+++ src/StataCall.jl
@@ -1,5 +1,23 @@
+__precompile__()
+
+# This package is not written for speed -- if you need to write fast code, you
+# should probably not use Stata in the first place. Instead, it should come in
+# handy if you would want to use some of Stata's cleaning routines on fairly
+# small DataFrames.
+
+include("setup.jl")
+
 module StataCall
 
-# package code goes here
+    using DataFrames
+
+    executable  = "/Applications/Stata/StataMP.app/Contents/MacOS/StataMP"
+
+    function StataCall(command::String)
+
+        # "C:\Program Files\Stata15\StataMP" /e do c:\data\bigjob.do
+
+    end
+
 
 end # module
---
+++ src/setup.jl
@@ -0,0 +1,60 @@
+# setup.jl
+
+@static if is_windows()
+
+    # TODO
+
+end
+
+@static if is_apple()
+
+    function locate_executable()
+
+        # try a few common directories
+        if isfile("/usr/bin/stata")
+            return "/usr/bin/stata"
+        elseif isdir("/Applications/Stata/StataSE.app")
+            appdir = "/Applications/Stata/StataSE.app"
+        elseif isdir("/Applications/Stata/StataMP.app")
+            appdir = "/Applications/Stata/StataMP.app"
+        elseif isdir("/Applications/Stata/StataIC.app")
+            appdir = "/Applications/Stata/StataIC.app"
+        elseif isdir("/Applications/StataSE.app")
+            appdir = "/Applications/StataSE.app"
+        elseif isdir("/Applications/StataMP.app")
+            appdir = "/Applications/StataMP.app"
+        elseif isdir("/Applications/StataIC.app")
+            appdir = "/Applications/StataIC.app"
+        else
+            appdir = ""
+        end
+
+        if (appdir != "") && (isfile(joinpath(appdir, "Contents", "MacOS", "StataMP")))
+            return joinpath(appdir, "Contents", "MacOS", "StataMP")
+        else
+
+            # try environment variables
+            if haskey(ENV,"STATA_BIN")
+                return ENV["STATA_BIN"]
+            end
+
+            # if we're still here, we have not found the executable
+            error("Could not find the Stata executable. Please set the
+                \"STATA_BIN\" environment variable or create a symlink
+                in /usr/bin/stata that points to the executable.")
+
+        end
+
+
+
+    end
+
+    const stata_executable = locate_executable()
+
+end
+
+@static if is_linux()
+
+    # TODO
+
+end
